5a792b6cae8f04658a98f1930bbe02960cfb4f78
Ayush Jain <ayushjain051001@gmail.com>
2021-03-23 23:29:25 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -12,6 +12,7 @@ try{
 
   //create a pdo instance
   $pdo = pg_connect($connStr);
+  
   $pod = new PDO($dsn, $user, $password);
   $pod->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
   $pod->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
