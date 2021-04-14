12f4632f6f609fbc09c066f25c31edd320766ac3
Ayush Jain <ayushjain051001@gmail.com>
2021-04-14 23:36:53 +0530

    Update signup.php

--- api/signup.php
+++ api/signup.php
@@ -9,7 +9,7 @@ header("HTTP/1.1 200 OK");
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-Wit
 
 $data = json_decode(file_get_contents('php://input'), true);
 
