f4a270d0ee9aef9c27599527fc24f712daf91ab5
Ayush Jain <ayushjain051001@gmail.com>
2021-04-23 22:26:46 +0530

    Add files via upload

---
+++ api/productpost.php
@@ -0,0 +1,90 @@
+<?php
+session_start();
+require "vendor/autoload.php";
+use \Firebase\JWT\JWT;
+
+include ('config/db.php');
+
+header("HTTP/1.1 200 OK");
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+pg_query($pdo, "set timezone='posix/Asia/Kolkata'");
+
+$authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+$arr = explode(" ", $authHeader);
+$secret_key = "AY05AS30YL31AC18";
+$jwt=$arr[1];
+
+try{
+    $decoded = JWT::decode($jwt, $secret_key, ['HS256']);
+    $now = new DateTimeImmutable();
+
+    if($jwt!=NULL && $decoded->iss=="https://flipin-store-api.herokuapp.com/" && $decoded->exp > $now->getTimestamp()){
+        $sidval = $decoded->data->id;
+        $variable = substr($sidval, 1, strlen($sidval));
+        $cid = (int)$variable;
+        $cat = substr($sidval, 0, 1);
+
+        if($cat=="C" || $cat=='C'){
+            $name = pg_escape_string($data['name']);
+            $desc = pg_escape_string($data['description']);
+            $category = pg_escape_string($data['category']);
+            $url = pg_escape_string($data['mediaUrl']);
+
+            if(trim($name)==""){
+                $nbud = array("responseCode"=>422, "error"=>"Invalid Item Name");
+                $value = json_encode($nbud);
+                echo("$value");
+            }
+            else if(trim($desc)=="")
+            {
+                $nbud = array("responseCode"=>422, "error"=>"Invalid Description");
+                $value = json_encode($nbud);
+                echo("$value");
+            }
+            else if(trim($category)=="" || ($category!="Furniture" && $category!="Jewellery" && $category!="Cosmetics" && $category!="Footwear" && $category!="Clothing" && $category!="Accessories"))
+            {
+                $nbud = array("responseCode"=>422, "error"=>"Invalid Category");
+                $value = json_encode($nbud);
+                echo("$value");
+            }
+            else if(trim($url)=="")
+            {
+                $nbud = array("responseCode"=>422, "error"=>"Invalid Media Url");
+                $value = json_encode($nbud);
+                echo("$value");
+            }
+            else{          
+                $insert = "Insert into productpost(productname, category, description, customerid, status, mediaurl, postviews) values(
+                    '$name','$category','$desc',$cid,'OPEN','$url',0);";
+                $datainsert = pg_query($pdo, $insert);
+
+                $finaldata = array( "responseCode" => 201,
+                                    "message"=> "Product Added Successfully.");
+                $value = json_encode($finaldata);
+                echo("$value");
+            }
+        }else{
+            echo json_encode(array(
+                "responseCode" => 403,
+                "message" => "Access Denied."
+            ));
+        }
+    }else{
+        echo json_encode(array(
+            "responseCode" => 403,
+            "message" => "Access denied."
+        ));
+    }
+}catch (Exception $e){
+    echo json_encode(array(
+        "responseCode" => 403,
+        "message" => "Access denied."
+    ));
+}
+?>
